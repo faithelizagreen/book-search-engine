09a4785fd973117e6606d2facd49876799a0d0dd
faithelizagreen <faithgreen@knights.ucf.edu>
2021-12-03 21:00:21 -0600

    fixed apollo server

--- server/server.js
+++ server/server.js
@@ -11,6 +11,11 @@ const { authMiddleWare } = require ('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,5 +28,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
